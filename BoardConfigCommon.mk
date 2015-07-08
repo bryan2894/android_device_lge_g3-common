095531519d1bd472694b6b7ca3b6f4a2daefc7dc
bryan2894 <bryan2894@gmail.com>
2015-07-07 22:16:55 -0400

    g3: disable odex compression
    Change-Id: Ia7c61a0ff7581fc4fb167c8fa9c59d43d992f6ce

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -72,6 +72,7 @@ ifeq ($(HOST_OS),linux)
   ifeq ($(TARGET_BUILD_VARIANT),user)
     ifeq ($(WITH_DEXPREOPT),)
       WITH_DEXPREOPT := true
+      WITH_DEXPREOPT_COMP := false
     endif
   endif
 endif
